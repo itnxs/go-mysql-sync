36dfcb5e7485930d2e70951ea41ab93487fd7ab4
lichenxin <lichenxin@juwang.cn>
2024-02-22 21:46:28 +0800

    删除无用

--- handler.go
+++ handler.go
@@ -1,23 +1,22 @@
 package gomysqlsync
 
 import (
-    "fmt"
-    "strings"
-    "sync"
+	"fmt"
+	"strings"
+	"sync"
 
-    "github.com/go-mysql-org/go-mysql/canal"
-    "github.com/go-mysql-org/go-mysql/mysql"
-    "github.com/go-mysql-org/go-mysql/replication"
-    "github.com/jmoiron/sqlx"
-    "github.com/pkg/errors"
-    "github.com/sirupsen/logrus"
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/go-mysql-org/go-mysql/replication"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // Message 消息内容
 type Message struct {
-    Action string                 `json:"action"`
-    Table  string                 `json:"table"`
-    Data   map[string]interface{} `json:"data"`
+	Action string                 `json:"action"`
+	Table  string                 `json:"table"`
+	Data   map[string]interface{} `json:"data"`
 }
 
 // OnMessage 消息处理
@@ -25,97 +24,90 @@ type OnMessage func(Message) error
 
 // eventHandler 事件处理
 type eventHandler struct {
-    canal.DummyEventHandler
-    logger    logrus.FieldLogger
-    db        *sqlx.DB
-    config    *Config
-    store     BinLogStore
-    tables    map[string]interface{}
-    mutex     sync.RWMutex
-    onMessage OnMessage
+	canal.DummyEventHandler
+	logger    logrus.FieldLogger
+	config    *Config
+	store     BinLogStore
+	tables    map[string]interface{}
+	mutex     sync.RWMutex
+	onMessage OnMessage
 }
 
 // newEventHandler 新建事件处理
-func newEventHandler(store BinLogStore, c *Config) (*eventHandler, error) {
-    db, err := sqlx.Open("mysql", c.MysqlDNS)
-    if err != nil {
-        return nil, errors.WithStack(err)
-    }
+func newEventHandler(store BinLogStore, config *Config) (*eventHandler, error) {
+	tables := make(map[string]interface{})
+	for _, table := range config.Tables {
+		table = strings.ToLower(fmt.Sprintf("%s.%s", config.driverConfig.DBName, table))
+		tables[table] = table
+	}
 
-    tables := make(map[string]interface{})
-    for _, table := range c.Tables {
-        table = strings.ToLower(fmt.Sprintf("%s.%s", c.driverConfig.DBName, table))
-        tables[table] = table
-    }
-
-    return &eventHandler{
-        logger: c.Logger,
-        db:     db,
-        config: c,
-        store:  store,
-        tables: tables,
-        onMessage: func(m Message) error {
-            c.Logger.WithField("data", m).Info("onMessage")
-            return nil
-        },
-    }, nil
+	return &eventHandler{
+		logger: config.Logger,
+		config: config,
+		store:  store,
+		tables: tables,
+		onMessage: func(m Message) error {
+			config.Logger.WithField("data", m).Info("onMessage")
+			return nil
+		},
+	}, nil
 }
 
 // checkTable 验证表名称
 func (h *eventHandler) checkTable(table string) bool {
-    if len(h.tables) <= 0 {
-        return true
-    }
-    _, ok := h.tables[table]
-    return ok
+	if len(h.tables) <= 0 {
+		return true
+	}
+	_, ok := h.tables[table]
+	return ok
 }
 
 // checkAction 验证数据更新操作类型
 func (h *eventHandler) checkAction(action string) bool {
-    switch action {
-    case canal.InsertAction, canal.UpdateAction, canal.DeleteAction:
-        return true
-    }
-    return false
+	switch action {
+	case canal.InsertAction, canal.UpdateAction, canal.DeleteAction:
+		return true
+	}
+	return false
 }
 
 // OnRow 数据更新处理
 func (h *eventHandler) OnRow(e *canal.RowsEvent) error {
-    table := strings.ToLower(e.Table.String())
-    if !h.checkAction(e.Action) {
-        h.logger.WithField("action", e.Action).Debug("action skip")
-        return nil
-    } else if !h.checkTable(table) {
-        h.logger.WithField("table", e.Table).Debug("table skip")
-        return nil
-    } else if len(e.Rows) <= 0 {
-        h.logger.WithField("table", e.Table).Debug("rows empty")
-        return nil
-    }
+	table := strings.ToLower(e.Table.String())
+	if !h.checkAction(e.Action) {
+		h.logger.WithField("action", e.Action).Debug("action skip")
+		return nil
+	} else if !h.checkTable(table) {
+		h.logger.WithField("table", e.Table).Debug("table skip")
+		return nil
+	} else if len(e.Rows) <= 0 {
+		h.logger.WithField("table", e.Table).Debug("rows empty")
+		return nil
+	}
 
-    row := e.Rows[len(e.Rows)-1]
-    data := make(map[string]interface{})
-    for i, c := range e.Table.Columns {
-        if vv, ok := row[i].([]byte); ok {
-            data[c.Name] = string(vv)
-        } else {
-            data[c.Name] = row[i]
-        }
-    }
+	row := e.Rows[len(e.Rows)-1]
+	data := make(map[string]interface{})
+	for i, c := range e.Table.Columns {
+		if vv, ok := row[i].([]byte); ok {
+			data[c.Name] = string(vv)
+		} else {
+			data[c.Name] = row[i]
+		}
+	}
 
-    return h.onMessage(Message{Action: e.Action, Table: table, Data: data})
+	return h.onMessage(Message{Action: e.Action, Table: table, Data: data})
 }
 
 // OnPosSynced 位置更新处理
 func (h *eventHandler) OnPosSynced(e *replication.EventHeader, pos mysql.Position, set mysql.GTIDSet, ok bool) error {
-    h.logger.WithField("position", pos).WithField("set", set).Debug("OnPosSynced")
-    if err := h.store.SetPosition(pos); err != nil {
-        return errors.WithStack(err)
-    }
-    if set != nil {
-        if err := h.store.SetGtIDSet(h.config.Flavor, set); err != nil {
-            return errors.WithStack(err)
-        }
-    }
-    return nil
+	h.logger.WithField("position", pos).WithField("set", set).Debug("OnPosSynced")
+	if err := h.store.SetPosition(pos); err != nil {
+		return errors.WithStack(err)
+	}
+	if set != nil {
+		if err := h.store.SetGtIDSet(h.config.Flavor, set); err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
 }
